f39e9c847c63c051264bf43b52744912a01da684
Tim Dronkers <tdronkers@gmail.com>
2022-08-24 12:51:18 -0600

    decrease text size for mobile

--- components/Bio.jsx
+++ components/Bio.jsx
@@ -9,12 +9,12 @@ function Bio() {
         <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-6xl">
           <div className="md:flex">
             <div className="md:shrink-0">
-              <img className="h-52 w-full object-cover md:h-full md:w-80" src="/assets/boys.webp" alt="tim and sons"/>
+              <img className="h-52 w-full object-cover md:h-full md:w-80" src="/assets/boys.webp" alt="tim and boys"/>
             </div>
             <div className="p-8">
-              <p className="mt-2 text-gray-700">I was born and raised in the San Francisco Bay Area (go Niners 🏈) but have called Colorado home for the last 10 years. I’m a husband and father of two boys. When I’m not coding or chasing my kids around, you can often find me outdoors, hopefully riding a snowboard 🏂 or bike 🚴‍♂️. If you enjoy Hunter S. Thompson’s writing I bet we would be fast friends.</p> 
+              <p className="mt-2 text-sm md:text-base text-gray-700">I was born and raised in the San Francisco Bay Area (go Niners 🏈) but have called Colorado home for the last 10 years. I’m a husband and father of two boys. When I’m not coding or chasing my kids around, you can often find me outdoors, hopefully riding a snowboard 🏂 or bike 🚴‍♂️. If you enjoy Hunter S. Thompson’s writing I bet we would be fast friends.</p> 
               <br/>
-              <p className="mt-2 text-gray-700">I started my professional life as an accountant but found my <a className="font-semibold text-[#011e6f] cursor-pointer" href="https://blog.dronkers.dev/buy-the-ticket-take-the-ride">true calling</a> in full-stack software development. I’m a problem-solver by nature and do my best work in collaboration with others. I love exploring new technologies but feel most at home in the JavaScript ecosystem.</p> 
+              <p className="mt-2 text-sm md:text-base text-gray-700">I started my professional life as an accountant but found my <a className="font-semibold text-[#011e6f] cursor-pointer" href="https://blog.dronkers.dev/buy-the-ticket-take-the-ride">true calling</a> in full-stack software development. I’m a problem-solver by nature and do my best work in collaboration with others. I love exploring new technologies but feel most at home in the JavaScript ecosystem.</p> 
             </div>
           </div>
         </div>
