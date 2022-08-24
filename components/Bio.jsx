d422ac5af471d53e98bcd79e28a229623ab09095
Tim Dronkers <tdronkers@gmail.com>
2022-08-24 10:39:23 -0600

    adding image to about page

--- components/Bio.jsx
+++ components/Bio.jsx
@@ -6,21 +6,19 @@ import boysImg from '../public/assets/boys.png'
 function Bio() {
   return (
     <div className='w-full h-screen text-center'>
-      <div className='max-w-xl w-full h-full mx-auto p-2 flex items-center'>
-        
-        <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex items-center'>
+
+        <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-6xl">
           <div class="md:flex">
             <div class="md:shrink-0">
-              <div id="profile-picture" className="image-container md:max-w-[250px]">
-              <Image layout="responsive" className="image h-48 w-full object-cover md:h-full md:w-48" src={boysImg} alt="tim and sons"/>
-              </div>
-              
+              <img class="h-48 w-full object-cover md:h-full md:w-48" src="/assets/boys.png" alt="Man looking at item at a store"/>
             </div>
             <div class="p-8">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>      
+              <p class="mt-2 text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget aliquet nibh praesent tristique magna. At risus viverra adipiscing at in tellus integer. Ut placerat orci nulla pellentesque dignissim. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Viverra nam libero justo laoreet sit amet cursus sit amet. Praesent elementum facilisis leo vel fringilla est. Mollis aliquam ut porttitor leo a diam sollicitudin tempor. Sapien nec sagittis aliquam malesuada bibendum arcu. In hac habitasse platea dictumst vestibulum rhoncus. Hac habitasse platea dictumst quisque sagittis purus sit amet.</p>
             </div>
           </div>
         </div>
+
       </div>
     </div>
   );
