91e579ea92f91561997cedcdf0edebfe2aa37884
Tim Dronkers <tdronkers@gmail.com>
2022-08-26 11:08:24 -0600

    edit about section

--- components/Bio.jsx
+++ components/Bio.jsx
@@ -12,12 +12,12 @@ function Bio() {
               <img className="h-52 w-full object-cover md:h-full md:w-80" src="/assets/boys.webp" alt="tim and boys" />
             </div>
             <div className="p-8">
-              <p className="mt-2 text-gray-700">I was born and raised in the San Francisco Bay Area (go Niners) but have called Colorado home for the last 10 years. I’m a husband and father of two boys. When I’m not coding or chasing my kids around, you can often find me outdoors, hopefully riding a snowboard 🏂 or bike 🚴‍♂️. If you enjoy Hunter S. Thompson’s writing I bet we would be fast friends.</p>
+              <p className="mt-2 text-gray-700">I was born and raised in the San Francisco Bay Area (go Niners) but have called Colorado home since 2013. I’m a husband and father of two boys. When I’m not coding or chasing my kids around, you can often find me outdoors, hopefully riding a snowboard 🏂 or bike 🚴‍♂️. If you enjoy Hunter S. Thompson’s writing I bet we would be fast friends.</p>
               <br />
               <p className="mt-2 text-gray-700">
                 I started my professional life as an accountant but found my
                 {' '}
-                <a className="font-semibold text-[#011e6f] cursor-pointer" href="https://blog.dronkers.dev/buy-the-ticket-take-the-ride">calling</a>
+                <a className="font-bold text-[#011e6f] cursor-pointer" href="https://blog.dronkers.dev/buy-the-ticket-take-the-ride">calling</a>
                 {' '}
                 in full-stack software development.
                 I’m a problem-solver by nature and do my best work in collaboration with others.
