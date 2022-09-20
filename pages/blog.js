26896346149caa5f855e624d68010e6db6c5c5c9
Tim Dronkers <tdronkers@gmail.com>
2022-09-20 15:15:52 -0600

    refactoring approach to looping through paginated gql data using async iterator

--- pages/blog.js
+++ pages/blog.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
 import React from 'react';
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import PostList from '../components/PostList';
@@ -12,56 +14,50 @@ export default function Blog({ posts }) {
   );
 }
 
-export async function getStaticProps() {
+async function* fetchBlogPosts() {
   const client = new ApolloClient({
     uri: 'https://api.hashnode.com',
     cache: new InMemoryCache(),
   });
+
+  let nextPageNumer = 0;
+
+  while (true) {
+    const { data } = await client.query({
+      query: gql`
+      query getPosts($page: Int) {
+        user(username: "tim-dronkers") {
+          publication {
+            posts(page: $page) {
+              _id
+              title
+              dateAdded
+              brief
+              totalReactions
+              responseCount
+            }
+          }
+        }
+      }
+      `,
+      variables: { page: nextPageNumer },
+    });
+
+    if (data.user.publication.posts.length === 0) {
+      break;
+    }
+    nextPageNumer += 1;
+    yield data.user.publication.posts;
+  }
+}
+
+export async function getStaticProps() {
   let results = [];
+  const iterator = fetchBlogPosts();
 
-  const { data } = await client.query({
-    query: gql`
-    query getPosts($page: Int) {
-      user(username: "tim-dronkers") {
-        publication {
-          posts(page: $page) {
-            _id
-            title
-            dateAdded
-            brief
-            totalReactions
-            responseCount
-          }
-        }
-      }
-    }
-    `,
-    variables: { page: 0 },
-  });
-
-  results = [...data.user.publication.posts];
-
-  const result2 = await client.query({
-    query: gql`
-    query getPosts($page: Int) {
-      user(username: "tim-dronkers") {
-        publication {
-          posts(page: $page) {
-            _id
-            title
-            dateAdded
-            brief
-            totalReactions
-            responseCount
-          }
-        }
-      }
-    }
-    `,
-    variables: { page: 1 },
-  });
-
-  results = results.concat([...result2.data.user.publication.posts]);
+  for await (const posts of iterator) {
+    results = results.concat(posts);
+  }
 
   return {
     props: {
