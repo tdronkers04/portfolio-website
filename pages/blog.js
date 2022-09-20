af2b5b1ed2e722543e7899038df2c40aed7c12ed
Tim Dronkers <tdronkers@gmail.com>
2022-09-20 15:42:41 -0600

    break out fetchPosts async iterator into separate module

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-restricted-syntax */
-/* eslint-disable no-await-in-loop */
 import React from 'react';
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import PostList from '../components/PostList';
+import fetchBlogPosts from '../lib/fetchAllBlogPosts';
 
 export default function Blog({ posts }) {
   return (
@@ -12,43 +11,6 @@ export default function Blog({ posts }) {
       </div>
     </div>
   );
-}
-
-async function* fetchBlogPosts() {
-  const client = new ApolloClient({
-    uri: 'https://api.hashnode.com',
-    cache: new InMemoryCache(),
-  });
-
-  let nextPageNumer = 0;
-
-  while (true) {
-    const { data } = await client.query({
-      query: gql`
-      query getPosts($page: Int) {
-        user(username: "tim-dronkers") {
-          publication {
-            posts(page: $page) {
-              _id
-              title
-              dateAdded
-              brief
-              totalReactions
-              responseCount
-            }
-          }
-        }
-      }
-      `,
-      variables: { page: nextPageNumer },
-    });
-
-    if (data.user.publication.posts.length === 0) {
-      break;
-    }
-    nextPageNumer += 1;
-    yield data.user.publication.posts;
-  }
 }
 
 export async function getStaticProps() {
