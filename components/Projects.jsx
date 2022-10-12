054619e7ce5732187c6bd3b9f351496cb0fbca56
Tim Dronkers <tdronkers@gmail.com>
2022-10-12 10:52:45 -0600

    remove unusued project components

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,23 +3,19 @@ import React from 'react';
 import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 import Trellific from './projects/Trellific';
 import GameAlert from './projects/GameAlert';
-// import HoneyDoList from './projects/HoneyDoList';
 import AirlineRoutes from './projects/AirlineRoutes';
 import Snaggl from './projects/Snaggl';
-// import TradingCards from './projects/TradingCards';
 
 function Projects() {
   return (
     <div id="projects" className="w-full px-4 mt-12 md:mt-4">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
-        <h2 className="border-t border-gray-700 py-12 text-gray-700 flex justify-center text-xl md:text-2xl">More Projects</h2>
+        <h2 className="border-t border-gray-700 py-12 text-gray-700 flex justify-center text-xl md:text-2xl">Other Projects</h2>
         <div className="grid md:grid-cols-2 gap-8">
           <Snaggl />
           <Trellific />
           <GameAlert />
           <AirlineRoutes />
-          {/* <HoneyDoList /> */}
-          {/* <TradingCards /> */}
         </div>
       </div>
       <div className="flex justify-center py-12">
